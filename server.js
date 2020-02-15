959bd20d999775eb3f1e5d67c5f25c0864949310
Pipek <zuzu.wc@gmail.com>
2020-02-15 11:54:01 +0700

    a is worthy

--- server.js
+++ server.js
@@ -9,22 +9,30 @@ let post = {
     }]
 }
 
+let test = []
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.get('/post', (req, res) => {
-    res.json(post)
-})
+// app.get('/post', (req, res) => {
+//     res.json(post)
+// })
 
 app.use(express.json()) //Use to parse JSON bodies
 app.use(express.urlencoded()) //Parse URL-endcoded bodies 
 
 
-app.post('/post', (req, res) => {
-    post.post.push(req.body)
-    res.status(201).json(req.body)
+// app.post('/post', (req, res) => {
+//     post.post.push(req.body)
+//     res.status(201).json(req.body)
+// })
+
+app.get('/post', (req, res) => {
+    test.push(req.query.a)
+    res.send(test)
 })
+
 
 
 app.listen(3000, () => {
